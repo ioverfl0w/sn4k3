0e5a3f7b2b37b9b5667945efd1469df9dba372bb
Wade Crickenberger <wadecxr@gmail.com>
2011-07-05 05:44:42 -0400

    made the colors fancier. dumb commit, really. but whatever haha.

--- src/org/wadec/snake/Snake.java
+++ src/org/wadec/snake/Snake.java
@@ -15,7 +15,7 @@ public class Snake extends Component implements KeyListener, Runnable {
     private int currentDirection = NORTH;
     private int currentHeading = currentDirection;
     private int[] currLocation;
-    private int[][] tailPlot = new int[25][2];
+    private int[][] tailPlot = new int[TAIL_PLOT_LEN][2];
 
     public Snake(int startX, int startY) {
         currLocation = new int[]{startX, startY};
@@ -33,31 +33,33 @@ public class Snake extends Component implements KeyListener, Runnable {
         //System.out.printf("d:%s\th:%s\n", d, h);
 
         //gets new header
-        if (isAcceptable(d,h))
+        if (isAcceptable(d, h)) {
             h = d;
+        }
 
         //generates new head plot
         int prevPlot[] = currLocation;
         switch (h) {
             case NORTH:
-                currLocation = new int[] {prevPlot[0], prevPlot[1] - MOVE_RATE};
+                currLocation = new int[]{prevPlot[0], prevPlot[1] - MOVE_RATE};
                 break;
             case SOUTH:
-                currLocation = new int[] {prevPlot[0], prevPlot[1] + MOVE_RATE};
+                currLocation = new int[]{prevPlot[0], prevPlot[1] + MOVE_RATE};
                 break;
             case EAST:
-                currLocation = new int[] {prevPlot[0] + MOVE_RATE, prevPlot[1]};
+                currLocation = new int[]{prevPlot[0] + MOVE_RATE, prevPlot[1]};
                 break;
             case WEST:
-                currLocation = new int[] {prevPlot[0] - MOVE_RATE, prevPlot[1]};
+                currLocation = new int[]{prevPlot[0] - MOVE_RATE, prevPlot[1]};
                 break;
         }
 
         //makes tail plot
         int[][] tail = new int[tailPlot.length][2];
         tail[0] = prevPlot;
-        for (int i = 1; i < tail.length; i++)
+        for (int i = 1; i < tail.length; i++) {
             tail[i] = tailPlot[i - 1];
+        }
 
         //declare new values
         currentHeading = h;
@@ -79,11 +81,14 @@ public class Snake extends Component implements KeyListener, Runnable {
             if (tailPlot[i][0] == -1) {
                 break;
             }
+            if (i > (tailPlot.length / 2)) {
+                g.setColor(Color.WHITE);
+            }
             g.fillRect(tailPlot[i][0] - 5, tailPlot[i][1] + 5, 10, 10);
         }
 
         //draw main box
-        g.setColor(Color.BLACK);
+        g.setColor(Color.RED);
         g.fillRect(currLocation[0] - 5, currLocation[1] + 5, 10, 10);
     }
 
@@ -125,6 +130,7 @@ public class Snake extends Component implements KeyListener, Runnable {
     public void keyReleased(KeyEvent e) {
     }
     public static final int MOVE_RATE = 10;
+    public static final int TAIL_PLOT_LEN = 15;
     public static final int NORTH = 0;
     public static final int EAST = 90;
     public static final int SOUTH = 180;
